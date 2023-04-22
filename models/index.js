f4993380a4b32a1d8fec7f8fb67883a1e9337afb
lyskz <lyski.zach@icloud.com>
2023-04-22 16:49:19 -0700

    got server able to run

--- models/index.js
+++ models/index.js
@@ -6,22 +6,22 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category',
+  foreignKey: 'category_id',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category',
+  foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
 
-// Products belongToMany Tags (through ProductTag)
+// // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   // foreignKey: 'product_tag',
 });
 
-// Tags belongToMany Products (through ProductTag)
+// // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   // foreignKey: 'product_tag',
