509256f6dcb617eac385d42ff457a4e4249e3e60
lyskz <lyski.zach@icloud.com>
2023-04-22 16:54:21 -0700

    updated models

--- models/index.js
+++ models/index.js
@@ -4,27 +4,35 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-});
-
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
 
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
 // // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  // foreignKey: 'product_tag',
+  foreignKey: 'product_id',
+  onUpdate: 'CASCADE',
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
 });
 
 // // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  // foreignKey: 'product_tag',
+  foreignKey: 'tag_id',
+  onUpdate: 'CASCADE',
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
 });
 
 module.exports = {
